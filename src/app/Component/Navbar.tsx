5c79995362b6af48fa448c314e5f7d9ef7a65a6f
Amber Asif <muhammadasif.qict@gmail.com>
2025-01-05 18:33:20 +0500

    2nd commit

--- src/app/Component/Navbar.tsx
+++ src/app/Component/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <div>
       <nav className="bg-slate-300 text-white h-12 py-2 px-3 flex justify-center text-center items-center">
         <div className="flex space-x-3 md:space-x-5 justify-center items-center text-center">
-           Amber Asif - Blog Content Creater
+            Blog Content Creater
           
           
         </div>
